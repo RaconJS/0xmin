94413ddfd1dd269001bfb9034d87a5cc00caf7cd
racon <46864506+RaconJS@users.noreply.github.com>
2022-07-10 19:26:25 +0100

    '#"tptasm";' more accessible build settings allows ',' in 'mov r0,r3' allows 'set' to be meta'#' when using tptasm starts adding '..secure'. starts Language class

--- collectFiles.sh
+++ collectFiles.sh
@@ -7,7 +7,8 @@ cp -r ./../code ./language
 cp -r ./../docs ./language
 cp -r ./../include ./language
 echo tpt
-cp ~/snap/the-powder-toy/current/.local/share/"The Powder Toy"/Saves/"a subframe tester.cps" the_powder_toy/Saves/
+cp ~/snap/the-powder-toy/current/.local/share/"The Powder Toy"/Saves/"a subframe tester.cps" the_powder_toy/Saves/"0xmin.cps"
+cp ~/snap/the-powder-toy/current/.local/share/"The Powder Toy"/Saves/"a_R216K2A.cps" the_powder_toy/Saves/"R216K2A.cps"
 echo sublime text
 rm -r syntax_for_sublimeText
 cp -r ~/./.config/sublime-text-3/Packages/0xmin syntax_for_sublimeText
