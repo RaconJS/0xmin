f402d09e8c6a9ffd72465de4be5b3690cf7317c2
racon <46864506+RaconJS@users.noreply.github.com>
2023-12-29 20:34:58 +0000

    idea: `label..constructor == 0xmin.class.Number` language proposal improves docs redoes JSON.js

--- language/compilers/JSON.js
+++ language/compilers/JSON.js
@@ -1,134 +1,131 @@
-let list=function deReference(object,debugVars=false){
-	let objs=[];//list of original objects
-	let refs=[];//objs->index pointers (semi compiled)
-	let index;
-	const add=function(value){
-		index=objs.length;
-		objs[index]=value;
-		refs[index]=({});
+function deReference(object){//:(any)->(unknown)[] ; is in prefix order
+	let refsGraph = [];//:(any & graph-like)[]; contains original objects
+	let refsTree = [];//: (any & tree-like)[]; contained cloned versions of the references structured like a lambda-calculus expression
+	let classIndexes = [];//:{name:String,index:number}[]
+	let isSearched = Symbol("isSearched");
+	function addItem(clonedValue,value){
+		const index = refsGraph.length;
+		refsGraph.push(value);
+		refsTree.push(clonedValue);
 		return index;
 	}
-	let refIndex=index=>index;//"::"+index+"::";
-	const addCaller=function(value,currentTree,returnFunc){
-		let o={
-			value:value,
-			//name:"",
-			//parent:null,
-			currentTree:currentTree,
-			returnFunc:returnFunc,
-		};
-		callBack.push(o);
-	}
-	const returnFuncGen=(objClone,i)=>v=>{objClone[i]=v;return v;};
-	const cloneStep=function({value,currentTree=[]}){
-		if(debugVars)trees.push(currentTree);
-		let objClone,index,i;
-		if(typeof value == "function"){
-			if(currentTree[currentTree.length-1]=="toJSON"){
-				return value;
-			}
-			return true?'('+value+')':'FUNCTION';
-		}
-		if(typeof value == "string"){
-			return JSON.stringify(value);
-		}
-		if(value === null)return '(null)';
-		if(typeof value == "symbol"){
-			index=objs.indexOf(value);
-			if(index!=-1){
-				return refIndex(index);
-			}else{
-				index=add(value);
-				return refIndex(index);
-			}
-		}
-		if(typeof value !== "object"){
-			return JSON.stringify(value);//"("+value+")";
-		}
-		index=objs.indexOf(value);
-		if(index!=-1){
-			return refIndex(index);
-		}
-		if("toJSON"in value){
-			if(typeof value.toJSON=="function"){
-				currentTree.push("toJSON()");
-				value=value.toJSON();
-			}
-			else{
-				currentTree.push("toJSON{}");
-			}
-		}
-		if(value instanceof Object){
-			index=add(value);
-			if(value instanceof Array)objClone=[];
-			else objClone={};
-			for(i in value){if(value.hasOwnProperty(i)){
-				currentTree.push(i);
-				addCaller(value[i],currentTree.map(v=>v),returnFuncGen(objClone,i));//objClone[i]=cloneStep(value[i]);
-				currentTree.pop();
-			}}
-			refs[index]=objClone;
-			return refIndex(index);//return(objClone);
-		}
-	}
-	let currentTree=[];
-	let callBack=[];
-	
-	let trees=[];
-	let obj2=[];
-	obj2.push(cloneStep({value:object}));
-	let value;
-	for(let i=0,i1=0;i<callBack.length&&i*0<10000;i++){
-		value=cloneStep(callBack[i]);
-		callBack[i].returnFunc(value);
-		if(debugVars)obj2.push(value);
-		else {delete callBack[i];}
-	}
-	return (!debugVars)?refs:{
-		objs,//list of different objects
-		refs,//objs.compile (mainOutput)
-		obj2,//list of all values
-		trees,//unneeded
-	};
-	//eval("("+v+")")
-	//JSON.stringify(JSON.list(mainGame).refs).length
-	//JSON.list(mainGame).trees.map(v=>v.join(" ")).join("\n").length
-};
-let parseList=function reReference(objectRefs,debugVars=false){
-	let object=[];
-	for(let i=0;i<objectRefs.length;i++){
-		for(let value in objectRefs[i]){
-			if(typeof objectRefs[i][value]=="number"){
-				objectRefs[i][value]=objectRefs[objectRefs[i][value]];
-			}
-			else if(typeof objectRefs[i][value]=="string"){
-				if(objectRefs[i][value][0]=="("){//is funtion?
-					let foo;
-					let eval1=(globalEval??eval);
-					let expression=objectRefs[i][value];
-					try{
-						foo=eval1(expression);
-					}catch(error){
-						try{
-							foo=eval1("(function "+expression.substr(1));
-						}catch(error){
-							//console.error("(function "+expression.substr(1));
-							//throw error;
-							foo="FUNCTION";
-						}
+	!function cloneValue(value){//:(any)->number & []refs*
+		let clonedValue = value;
+		let index;
+		if(refsGraph.includes(value))
+			return refsGraph.indexOf(value);
+		switch (typeof value){
+			case"object":
+				let object = value;
+				if(object.toJSON){
+					object = object.toJSON();
+				}
+				let classObj;
+				let prototype = Object.getPrototypeOf(object);
+				if(!Object.hasOwn(object,"constructor")){
+					classObj = object.constructor;
+				}
+				else if(!Object.hasOwn(prototype,"constructor")){
+					classObj = prototype.constructor;
+				}
+				else throw Error("Could not find the class name of object")
+				let classIndex;
+				function handleOtherClass(){
+					if(refsGraph.includes(classObj)){
+						classIndex = refsGraph.indexOf(classObj);
 					}
-					objectRefs[i][value]=foo;
+					else{
+						classIndex = refsGraph.push(classObj) - 1;
+						refsTree.push([]);//:index[] ; contains references to its instance objects
+						//assert: refsGraph((refsTree[classIndex])[ x ]) instanceof classObj
+						classIndexes.push([classObj.name,classIndex]);
+					}
+					refsTree[classIndex].push(refsGraph.length);//push the index of this object
+					
 				}
-				else{
-					window.things=objectRefs[i][value]
-					objectRefs[i][value]=JSON.parse(objectRefs[i][value]);
+				switch(classObj){
+					case Set:
+					case Map://json strings do not support maps and sets
+						handleOtherClass();
+					case Array:
+						clonedValue = [];
+						for(let item of object){
+							clonedValue.push(cloneValue(item));
+						}
+						break;
+					break;
+					default:
+						handleOtherClass();
+					case Object:
+						clonedValue = {};
+						index = addItem(clonedValue,object);
+						for(let property in object){
+							if(Object.hasOwn(object,property)){
+								clonedValue[property] = cloneValue(object[property]);
+							}
+						}
+						let symbols = [];//[Symbol,tree-like value or object][]
+						clonedValue = [classObj.name+"",clonedValue,symbols];//[String,tree-like,[Symbol,tree-like][]]
+						for(let symbol of Object.getOwnPropertySymbols(object)){
+							symbols.push([cloneValue(symbol), cloneValue(object[symbol])]);
+						}
+						if(symbols.length == 0)clonedValue = clonedValue[1];//get back the object on its own
+						refsTree[index] = clonedValue;
+					break;
+				}
+				return index;
+			case"number":
+			case"string":
+			case"symbol":
+			case"function":
+				return addItem(clonedValue,value);
+			break;
+		}
+	}(object);
+	return {
+		originals:refsGraph,
+		cloned:{refs:refsTree,classIndexes},
+		classes:classIndexes,
+	};
+}
+function parseList({refs,classIndexes},classList){
+	//assume prefix order
+	function handleObject(reference,i){
+		for(let i in reference){
+			if(Object.hasOwn(reference,i)){
+				if(typeof reference[i] == "number"){
+					reference[i] = refs[reference[i]];
 				}
 			}
 		}
 	}
-	return debugVars?objectRefs:objectRefs[0];
+	for(let i = refs.length - 1; i >= 0; i--){//reference:number|Object|Array|
+		let reference = refs[i];
+		if(typeof reference == "number"){
+			continue;
+		}
+		else if(reference instanceof Array && typeof reference[0] == "string"){
+			handleObject(reference[1],i);
+			for(let [symbol,value] of reference[2]){
+				if(typeof reference[1][symbol] == "number"){
+					reference[1][symbol] = refs[reference[1][i]];
+				}
+				else reference[1][symbol] = reference[1][i];
+			}
+			refs[i] = reference[1];
+		}
+		if(reference.constructor == Object || reference instanceof Array){
+			handleObject(reference,i);
+		}
+	}
+	//refs[i] = Object.assign(new classList[i](),reference[1]);
+	return refs[0];
 }
-module.exports={
-	listify:list,
+let a = {a:5};
+let b = {a,c:new class asd{}};
+a.b = b;
+console.log(parseList(deReference(a).cloned,[new class asd{}]));
+if(0)module.exports={
+	listify:deReference,
 	parseList:parseList,
 };
